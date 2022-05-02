00ffb82750cef71625199f5b4e0e3f8393a8a496
Qifan Yang <14819756+underwindfall@users.noreply.github.com>
2022-05-02 11:30:56 +0200

    rename package name

--- plugin-build/buildSrc/src/main/java/Coordinates.kt
+++ plugin-build/buildSrc/src/main/java/Coordinates.kt
@@ -1,13 +1,13 @@
 object PluginCoordinates {
-    const val ID = "com.qifan.zeplin.gradle"
-    const val GROUP = "com.qifan"
+    const val ID = "io.github.qifan.zeplin.gradle"
+    const val GROUP = "io.github.qifan"
     const val VERSION = "0.0.1"
-    const val   IMPLEMENTATION_CLASS = "com.qifan.zeplin.gradle.ZeplinDownloadPlugin"
+    const val IMPLEMENTATION_CLASS = "com.qifan.zeplin.gradle.ZeplinDownloadPlugin"
 }
 
 object PluginBundle {
-    const val VCS = "https://github.com/underwindfall/gradle-zepling-asset-downloader.git"
-    const val WEBSITE = "https://github.com/underwindfall/gradle-zepling-asset-downloader"
+    const val VCS = "https://github.com/underwindfall/zeplin-asset-download-gradle.git"
+    const val WEBSITE = "https://github.com/underwindfall/zeplin-asset-download-gradle"
     const val DESCRIPTION = "A plugin used to download assets from zeplin and convert it to useful assets (vector-drawables) for android"
     const val DISPLAY_NAME = "ZeplinAssetDownloader"
     val TAGS = listOf(
